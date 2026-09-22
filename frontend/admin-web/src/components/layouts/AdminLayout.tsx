99365096fccfe9ac39ad8855cbd50aee3b3a4f5f
mitklsenpai <tran.tuyen25062006@gmail.com>
2026-09-22 08:47:38 +0700

    feat: create layout, and config Sidebar routes

--- frontend/admin-web/src/components/layouts/AdminLayout.tsx
+++ frontend/admin-web/src/components/layouts/AdminLayout.tsx
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useMatches } from "react-router-dom"
+import { Sidebar } from "./Sidebar"
+import { Header } from "./Header"
 
-export default function AdminLayout () {
+
+export default function AdminLayout() {
+    const matches = useMatches()
+    const title = (matches.at(-1)?.handle as { title?: string })?.title ?? 'Nothing'
+    
     return (
-        <div> 
-            Đây là AdminLayout
-            <Outlet />
+        <div className="flex h-screen bg-neutral-50">
+            <Sidebar />
+            <div className="flex flex-1 flex-col">
+                <Header title={title} />
+                <main className="flex-1 overflow-y-auto p-6">
+                    <Outlet />
+                </main>
+            </div>
         </div>
     )
 }
--- frontend/admin-web/src/routes/protected-route.tsx
+++ frontend/admin-web/src/routes/protected-route.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom"
 import AdminLayout from "../components/layouts/AdminLayout"
 
-export default function ProtecedRoute() {
+export default function ProtectedRoute() {
     const token = true  // Them hook lay token nguoi dung sau :(
     return token ? <AdminLayout /> : <Navigate to="/login" replace />
 }
