9016cb90ea3636ec7b47de5da0da86e25ece7a7b
canknotcawh <dangconganh999@gmail.com>
2026-09-25 21:24:26 +0700

    feat: update application configuration, add user controller, and create identity tables with roles

--- backend/src/lms/src/main/java/com/soa/gr6/lms/LmsApplication.java
+++ backend/src/lms/src/main/java/com/soa/gr6/lms/LmsApplication.java
@@ -2,12 +2,13 @@ package com.soa.gr6.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class LmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LmsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        SpringApplication.run(LmsApplication.class, args);
+    }
 }
